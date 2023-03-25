b79813e50cfe912816322240c51492fd419f2577
Ana <ing.agsreyna19@gmail.com>
2023-03-25 16:47:49 -0600

    Update example

--- 00_Introduccion/ejercicio_1.py
+++ 00_Introduccion/ejercicio_1.py
@@ -26,6 +26,6 @@ print(type(2.5 * 1.5 * 3))              # <class 'float'>
 print(type(2j + 2 + 3j + 5))            #
 print(type('Hola mundo'))               # 
 print(type(['Mar', 2, 5]))              # 
-print(type({'Nombre':'Tu nombre'}))     # 
+print(type({'Nombre':'Tu nombre'}))     # <class 'dict'>
 print(type({'Algo', 2.67, 9.5}))        # <class 'set'>
 print(type(('Lag', 6.8, 7.5, 9)))       # <class 'tuple'>
