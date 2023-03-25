20f4564c6b6f402d304ce6fa2acc18af40331175
Ana <ing.agsreyna19@gmail.com>
2023-03-25 16:10:41 -0600

    Update example

--- 00_Introduccion/ejercicio_1.py
+++ 00_Introduccion/ejercicio_1.py
@@ -1,12 +1,12 @@
 #Ejercicios 1
-#ESTO ES UNA PRUEBAAAAAA222!!!3333
+# HOLA PRueba de cambios
 # Introducción
 # Una expresión puede ser una operación aritmetica
 
 print(6 + 9)                       # 15
-print(3 - 1)                       # 
-print(1 * 1)                       # 
-print(8 / 5)                       # 
+print(3 - 1)                       # 2
+print(1 * 1)                       # 1
+print(8 / 5)                       # 1.6
 print(2 ** 4)                      # 
 print(8 % 3)                       # 
 print(9 // 4)                      # 
@@ -22,10 +22,10 @@ print(('Lag', 6.8, 7.5, 9))        #
 # Tipos de datos
 
 print(type(15<6))                       # <class 'bool'>
-print(type(2.5 * 1.5 * 3))              #
+print(type(2.5 * 1.5 * 3))              # <class 'float'>
 print(type(2j + 2 + 3j + 5))            #
 print(type('Hola mundo'))               # 
-print(type(['Mar', 2, 5]))            # 
+print(type(['Mar', 2, 5]))              # 
 print(type({'Nombre':'Tu nombre'}))     # 
 print(type({'Algo', 2.67, 9.5}))        # 
 print(type(('Lag', 6.8, 7.5, 9)))       # 
