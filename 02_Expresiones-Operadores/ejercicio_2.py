685b848b27c515e69047e4ad6023c72ae30f9139
Ana <ing.agsreyna19@gmail.com>
2023-03-25 19:23:32 -0600

    Update example

--- 02_Expresiones-Operadores/ejercicio_2.py
+++ 02_Expresiones-Operadores/ejercicio_2.py
@@ -1,7 +1,7 @@
 # Expresiones y Operadores
 
 ## Expresiones.
-
+'''
 Las expresiones son combinaciones de constantes, variables, símbolos de operación, paréntesis y nombres de funciones especiales.
 
 Una expresión consta de operandos y operadores. Según sea el tipo de objetos que manipulan, las expresiones se clasifican en: 
@@ -23,12 +23,12 @@ Las expresiones aritméticas son análogas a las fórmulas matemáticas. Las var
 * `**` - Este es el operador de exponencial y permite obtener la potencia de un operando en función de otro operando.
 
 A continuación se presentan algunos ejemplos de los operadores en código Python.
-
+'''
 ### Operador +.
 
 #python
 a = 7 + 3
-print(a)
+print(a)   #10 class
 a = 5
 b = 3
 c = a + b
@@ -99,18 +99,6 @@ print(c)
 print(4**3)
 #
 
-Expresiones relacionales
-
-* `<` - Este es el operador de menor que, y se utiliza en las expresiones relacionales para saber si un operando es menor a otro operando.
-* `>` - Este es el operador de mayor que, y se utiliza en las expresiones relacionales para saber si un operando es mayor a otro operando.
-* `==` - Este es el operador de igual que, y se utiliza en las expresiones relacionales para saber si un operando es igual a otro operando.
-* `!=` - Este es el operador desigual que, y se utiliza en las expresiones relacionales para saber si un operando es diferente a otro operando.
-* `<=` - Este es el operador de menor igual que, y se utiliza en las expresiones relacionales para saber si un operando es menor o igual a otro operando.
-* `>=` Este es el operador de mayor igual que, y se utiliza en las expresiones relacionales para saber si un operando es mayor igual a otro operando.
-
-NOTA: Como resultado de la aplicación de las expresiones relacionales, se obtendrá como resultado una salida booleana, es decir `TRUE` o `FALSE`.
-
-A continuación se presentan algunos ejemplos de las expresiones relacionales en código Python.
 
 ### Operador <.
 
@@ -189,10 +177,6 @@ print(7>=3)
 ## Expresiones lógicas.
 
 
-* `not` - Este es el operador lógico para realizar una negación, si el operando no se encuentra en el otro operando, la expresión se cumple y resulta en True, caso contrario, si la expresión no se cumple el resultado es False.
-* `and` - Este es el operador lógico para realizar una conjugación, si cualquiera de los operandos presentes no se cumple en la expresión lógica, esta resulta en False, caso contrario, si toda la expresión lógica de conjugación se cumple el resultado es True.
-* `or` - Este es el operador lógico para realizar una conjugación, si cualquiera de los operandos presentes no se cumple en la expresión lógica, esta resulta en False, caso contrario, si toda la expresión lógica de conjugación se cumple el resultado es True.
-
 ### Operador and.
 
 #python
@@ -216,11 +200,7 @@ print(not 5>4)
 
 ## Expresiones de carácter
 
-A diferencia de las demás expresiones no existe un operador estático sino una búsqueda de secuencias, números o caracteres dentro de una variable.
-
-Estas expresiones de búsqueda comúnmente llamadas expresiones regulares, sirven para captar ciertos elementos o patrones dentro de un valor.
-
-Ejemplo:
+#Ejemplo:
 
 #python
 import re
@@ -228,3 +208,6 @@ frase = "Tengo 2 hijos que tienen 15 y 11 años"
 patron = '[0-9]+' #Esta es una expresión regular
 re.findall(patron, frase)
 #
+
+
+
--- 03_Cadenas/ejemplo_3.py
+++ 03_Cadenas/ejemplo_3.py
@@ -1,2 +1,13 @@
-a='Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.'.replace('Celsius', 'C')
-print(a)
+#fact = 'Esto es una Cadena'
+#print(fact)
+#fact_two = fact + ' esto es otra cadena'
+#print(fact_two)
+
+
+#a='Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.'.replace('Celsius', 'C')
+#print(a)
+
+#print('temperatures and facts about the moon'.title())
+
+subject = 'interesting facts about the moon'
+print(f'{subject.title()}')
